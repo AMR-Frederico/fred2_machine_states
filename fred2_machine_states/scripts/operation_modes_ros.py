4156875fa99b4426df837619261f9e0d2f3f0b14
alecremer <ale.cremer.dev@gmail.com>
2024-05-13 21:12:07 -0300

    Separe ros python, add enum

--- fred2_machine_states/scripts/operation_modes_ros.py
+++ fred2_machine_states/scripts/operation_modes_ros.py
@@ -1,20 +1,62 @@
 #!/usr/bin/env python3
 
+import rclpy
+import threading
 import sys
-
-from enum import Enum
 
 from typing import List
 
+from rclpy.context import Context
+from rclpy.executors import SingleThreadedExecutor
+from rclpy.node import Node, ParameterDescriptor
+from rclpy.parameter import Parameter, ParameterType
+from rclpy.qos import QoSPresetProfiles, QoSProfile, QoSHistoryPolicy, QoSLivelinessPolicy, QoSReliabilityPolicy, QoSDurabilityPolicy
 
-class OperationStates(Enum):
+from rcl_interfaces.msg import SetParametersResult
 
-    INIT = 0
-    MANUAL_MODE = 10
-    AUTONOMOUS_MODE = 20
-    EMERGENCY_MODE = 30
+from std_msgs.msg import Bool, Int16
 
-class OperationModesStateMachine():
+
+debug_mode = '--debug' in sys.argv
+
+class GenericCallback():
+
+
+    def callback(self, ref, default_value, callback_1= None, callback_2= None, callback_3= None):
+
+        ref['value'] = default_value
+        return lambda msg: [self._set(ref, msg.data), 
+                            self._execute_if_not_none(callback_1), 
+                            self._execute_if_not_none(callback_2), 
+                            self._execute_if_not_none(callback_3)]
+
+
+    def data_declare(self, default_value = {}):
+
+        ref = {}
+        ref['value'] = default_value
+        return ref
+
+
+    def _set(self, ref, value):
+            
+        ref['value'] = value
+
+
+    def _execute_if_not_none(self, function):
+            
+        if function is not None: 
+            
+            function()
+
+
+    def get(self, ref):
+
+        return ref['value']
+
+
+
+class Fred_state(Node):
 
     
 
@@ -22,11 +64,42 @@ class OperationModesStateMachine():
     #    Node Setup
     # --------------------------------------------------------------------------------
         
-    def __init__(self):
+    def __init__(self,
+                 node_name: str,
+                 *,
+                 context:
+                 Context = None,
+                 cli_args: List[str] = None,
+                 namespace: str = None,
+                 use_global_arguments: bool = True,
+                 enable_rosout: bool = True,
+                 start_parameter_services: bool = True,
+                 parameter_overrides: List[Parameter] = None,
+                 allow_undeclared_parameters: bool = False,
+                 automatically_declare_parameters_from_overrides: bool = False) -> None:
 
 
-        self.state = OperationStates.INIT.value
+        super().__init__(node_name,
+                         context=context,
+                         cli_args=cli_args,
+                         namespace=namespace,
+                         use_global_arguments=use_global_arguments,
+                         enable_rosout=enable_rosout,
+                         start_parameter_services=start_parameter_services,
+                         parameter_overrides=parameter_overrides,
+                         allow_undeclared_parameters=allow_undeclared_parameters,
+                         automatically_declare_parameters_from_overrides=automatically_declare_parameters_from_overrides)
 
+
+        # quality protocol -> the node must not lose any message 
+        qos_profile = QoSProfile(
+            reliability=QoSReliabilityPolicy.RELIABLE, 
+            durability= QoSDurabilityPolicy.VOLATILE,
+            history=QoSHistoryPolicy.KEEP_LAST, 
+            depth=10, 
+            liveliness=QoSLivelinessPolicy.AUTOMATIC
+            
+        )
 
         self.last_change_mode = False
         self.switch_mode = False
@@ -62,6 +135,30 @@ class OperationModesStateMachine():
         self.robot_safety = GenericCallback.data_declare(False)
 
 
+        # --------------------------------------------------------------------------------
+        #    Subscribers
+        # --------------------------------------------------------------------------------
+
+        # Switch mode
+        self.create_subscription(Bool,
+                                 '/joy/machine_states/switch_mode',
+                                 self.generic_callback.callback(self.change_mode, False),
+                                 qos_profile)
+
+        # Robot safety
+        self.create_subscription(Bool,
+                                 '/robot_safety',
+                                 self.generic_callback.callback(self.robot_safety, False, self.update_safe_time),
+                                 qos_profile)
+
+
+
+        # --------------------------------------------------------------------------------
+        #    Publishers
+        # --------------------------------------------------------------------------------
+        self.operation_state_pub = self.create_publisher(Int16, 'operation_state', qos_profile)
+
+
 
 
         self.add_on_set_parameters_callback(self.parameters_callback)
@@ -73,7 +170,41 @@ class OperationModesStateMachine():
     # --------------------------------------------------------------------------------
     #    Class Functions
     # --------------------------------------------------------------------------------
-   
+    
+    def load_ros_param(self):
+    
+        """Load params from to ROS param server 
+        """
+        
+        # Declare parameters related to robot states and debug/testing
+        self.declare_parameters(
+            namespace='',
+            parameters=[
+                ('emergency', 0, ParameterDescriptor(description='Index for EMERGENCY state', type=ParameterType.PARAMETER_INTEGER)),
+                ('manual', 10, ParameterDescriptor(description='Index for MANUAL state', type=ParameterType.PARAMETER_INTEGER)),
+                ('autonomous', 20, ParameterDescriptor(description='Index for AUTONOMOUS state', type=ParameterType.PARAMETER_INTEGER)),
+                ('debug', False, ParameterDescriptor(description='Enable debug prints', type=ParameterType.PARAMETER_BOOL))
+            ]
+        )
+
+        self.get_logger().info('All parameters set successfully')
+
+
+    def get_ros_params(self):
+        
+        """Load params from ros param server to internal class
+        """
+        self.MANUAL = self.get_parameter('manual').value
+        self.AUTONOMOUS = self.get_parameter('autonomous').value
+        self.EMERGENCY = self.get_parameter('emergency').value
+        self.DEBUG = self.get_parameter('debug').value
+
+
+        # robot mode (MANUAL | AUTONOMOUS)  # robot state (EMERGENCY | IN GOAL | MISSION COMPLETED | MANUAL | AUTONOMOUS)
+        self.robot_state = self.EMERGENCY    # Starts in EMERGENCY state 
+        self.robot_mode = self.MANUAL     # Starts in MANUAL mode 
+
+
 
 
 
