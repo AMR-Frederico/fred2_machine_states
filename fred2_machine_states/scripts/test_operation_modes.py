4d5fc9f0e6a39ed54e66265b1861eeba0e380224
alecremer <ale.cremer.dev@gmail.com>
2024-05-13 22:13:31 -0300

    Fix test autonomous to emergency

--- fred2_machine_states/scripts/test_operation_modes.py
+++ fred2_machine_states/scripts/test_operation_modes.py
@@ -20,7 +20,7 @@ def test_started():
     switch_mode = False
     robot_safe = True
 
-    state_machine.machine_states_routine(switch_mode, robot_safe)
+    state_machine.machine_states_routine(robot_safe, switch_mode)
 
     assert state_machine.state == OperationStates.MANUAL_MODE
 
@@ -32,8 +32,8 @@ def test_manual_to_autonomous():
     switch_mode = True
     robot_safe = True
 
-    state_machine.machine_states_routine(switch_mode, robot_safe)
-    state_machine.machine_states_routine(switch_mode, robot_safe)
+    state_machine.machine_states_routine(robot_safe, switch_mode)
+    state_machine.machine_states_routine(robot_safe, switch_mode)
 
     assert state_machine.state == OperationStates.AUTONOMOUS_MODE
 
@@ -45,9 +45,9 @@ def test_autonomous_to_manual():
     switch_mode = True
     robot_safe = True
 
-    state_machine.machine_states_routine(switch_mode, robot_safe)
-    state_machine.machine_states_routine(switch_mode, robot_safe)
-    state_machine.machine_states_routine(switch_mode, robot_safe)
+    state_machine.machine_states_routine(robot_safe, switch_mode)
+    state_machine.machine_states_routine(robot_safe, switch_mode)
+    state_machine.machine_states_routine(robot_safe, switch_mode)
 
     assert state_machine.state == OperationStates.MANUAL_MODE
 
@@ -59,7 +59,7 @@ def test_init_to_emergency():
     switch_mode = False
     robot_safe = False
 
-    state_machine.machine_states_routine(switch_mode, robot_safe)
+    state_machine.machine_states_routine(robot_safe, switch_mode)
 
     assert state_machine.state == OperationStates.EMERGENCY_MODE
 
@@ -71,11 +71,11 @@ def test_manual_to_emergency():
     switch_mode = False
     robot_safe = True
 
-    state_machine.machine_states_routine(switch_mode, robot_safe)
+    state_machine.machine_states_routine(robot_safe, switch_mode)
 
     robot_safe = False
 
-    state_machine.machine_states_routine(switch_mode, robot_safe)
+    state_machine.machine_states_routine(robot_safe, switch_mode)
 
     assert state_machine.state == OperationStates.EMERGENCY_MODE
 
@@ -87,8 +87,12 @@ def test_autonomous_to_emergency():
     switch_mode = True
     robot_safe = True
 
-    state_machine.machine_states_routine(switch_mode, robot_safe)
-    state_machine.machine_states_routine(switch_mode, robot_safe)
-    state_machine.machine_states_routine(switch_mode, robot_safe)
+    state_machine.machine_states_routine(robot_safe, switch_mode)
+    state_machine.machine_states_routine(robot_safe, switch_mode)
+
+    switch_mode = True
+    robot_safe = False
+
+    state_machine.machine_states_routine(robot_safe, switch_mode)
 
     assert state_machine.state == OperationStates.EMERGENCY_MODE
