f406147cadab38695568242cf651389bff622187
alecremer <ale.cremer.dev@gmail.com>
2024-05-15 00:27:58 -0300

    add operation modes callbacks

--- fred2_machine_states/scripts/operation_modes.py
+++ fred2_machine_states/scripts/operation_modes.py
@@ -12,13 +12,25 @@ class OperationStates(Enum):
     AUTONOMOUS_MODE = 20
     EMERGENCY_MODE = 30
 
+def execute_if_not_none(function):
+
+    if function is not None:
+
+        function()
 
 
 class OperationModesStateMachine():
 
-    def __init__(self, init_callback = None):
+    def __init__(self, 
+                init_callback = None,
+                manual_mode_callback = None,
+                autonomous_mode_callback = None,
+                emergency_callback = None):
 
         self.init_callback = init_callback
+        self.manual_mode_callback = manual_mode_callback
+        self.autonomous_mode_callback = autonomous_mode_callback
+        self.emergency_callback = emergency_callback
 
 
         # -------------------------------------
@@ -98,10 +110,25 @@ class OperationModesStateMachine():
 
             case OperationStates.INIT:
 
-                if self.init_callback is not None:
-                    self.init_callback()
+                execute_if_not_none(self.init_callback)
                 
                 self.initialized = True
+
+
+            case OperationStates.MANUAL_MODE:
+
+                execute_if_not_none(self.manual_mode_callback)
+
+            
+            case OperationStates.AUTONOMOUS_MODE:
+
+                execute_if_not_none(self.autonomous_mode_callback)
+
+
+            case OperationStates.EMERGENCY_MODE:
+
+                execute_if_not_none(self.emergency_callback)
+                
 
         # -------------------------------------
         #    Reset variables
