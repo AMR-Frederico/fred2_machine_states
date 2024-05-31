617ca9e62b658b6f42647646427c07d88f1ad76d
alecremer <ale.cremer.dev@gmail.com>
2024-05-31 15:54:39 -0300

    Add fuzz example tructure

--- fred2_machine_states/scripts/check_if_at_waypoint_helper.py
+++ fred2_machine_states/scripts/check_if_at_waypoint_helper.py
@@ -1,3 +1,75 @@
+import numpy as np
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
+from enum import Enum
+
+
+# --------------------------------------------------------------------------------
+#    Problema
+# --------------------------------------------------------------------------------
+#   Sensor indutivo -> 0, 1
+#   Sensor de cor -> RGBA []
+#   Odometria -> posicao [x, y, yaw]
+#
+
+class Names(Enum):
+
+    # Spaces
+    D_WAYPOINT_ODOM = "d_waypoint_odom"
+    INDUCTIVE_SIGNAL = "inductive_signal"
+    COLOR_SIGNAL = "color_signal"
+    AT_WAYPOINT = "at_waypoint"
+    
+    # Qualities
+    AT = "at"
+    NEAR = "near"
+    FAR = "far"
+
+    METAL = "metal"
+    NOT_METAL = "not_metal"
+
+    Y_100 = "y 100%"
+    Y_0 = "y 0%"
+
+in_distance_waypoint_odom = ctrl.Antecedent(np.arange(0, 100, 0.1), Names.D_WAYPOINT_ODOM)
+in_inductive_signal = ctrl.Antecedent(np.arange(0, 2, 1), Names.INDUCTIVE_SIGNAL)
+in_color_signal = ctrl.Antecedent(np.arange(0, 2, 0.01), Names.COLOR_SIGNAL)
+
+out_at_waypoint = ctrl.Consequent(np.arange(0, 100, 1), Names.AT_WAYPOINT)
+
+# automf(Ok, ... , Not Ok)
+in_distance_waypoint_odom.automf(number=3, names=[Names.AT, Names.NEAR, Names.FAR])
+in_inductive_signal.automf(number=2, names=[Names.METAL, Names.NOT_METAL])
+in_color_signal.automf(number=2, names=[Names.Y_100, Names.Y_0])
+
+out_at_waypoint[Names.FAR] = fuzz.trimf(out_at_waypoint.universe, [0, 25, 50])
+out_at_waypoint[Names.NEAR] = fuzz.trimf(out_at_waypoint.universe, [25, 50, 75])
+out_at_waypoint[Names.AT] = fuzz.trimf(out_at_waypoint.universe, [50, 75, 100])
+
+
+# in_distance_waypoint_odom.view()
+# in_inductive_signal.view()
+# in_color_signal.view()
+
+rules = []
+
+rules.append(ctrl.Rule(in_distance_waypoint_odom[Names.AT] and in_inductive_signal[Names.METAL], out_at_waypoint[Names.AT]))
+rules.append(ctrl.Rule(in_distance_waypoint_odom[Names.NEAR] and in_inductive_signal[Names.METAL], out_at_waypoint[Names.AT]))
+rules.append(ctrl.Rule(in_distance_waypoint_odom[Names.FAR] and in_inductive_signal[Names.METAL], out_at_waypoint[Names.FAR]))
+rules.append(ctrl.Rule(in_distance_waypoint_odom[Names.AT] and in_inductive_signal[Names.NOT_METAL], out_at_waypoint[Names.NEAR]))
+rules.append(ctrl.Rule(in_distance_waypoint_odom[Names.NEAR] and in_inductive_signal[Names.NOT_METAL], out_at_waypoint[Names.FAR]))
+rules.append(ctrl.Rule(in_distance_waypoint_odom[Names.FAR] and in_inductive_signal[Names.NOT_METAL], out_at_waypoint[Names.FAR]))
+
+# out_at_waypoint.view()
+
+# at_waypoint_system = ctrl.ControlSystem(rules)
+
+# at_waypoint = ctrl.ControlSystemSimulation(at_waypoint_system)
+r = ctrl.Rule(in_distance_waypoint_odom[Names.AT] and in_inductive_signal[Names.METAL], out_at_waypoint[Names.AT])
+r.view()
+
+input("Wait")
+
 def color_to_value(color):
 
     color_sum = color[0] + color[1] - color[2]
